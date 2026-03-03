cb6cd608396e4b06115467e39cb707240ab5b94b
YohnnD <grabieldiaz63@gmail.com>
2026-03-03 15:25:01 -0400

    Initial commit

---
+++ src/main/java/com/tu_nombre/scaffold/SpringKeycloakScaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.tu_nombre.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeycloakScaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKeycloakScaffoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tu_nombre/scaffold/SpringKeycloakScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tu_nombre.scaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKeycloakScaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
